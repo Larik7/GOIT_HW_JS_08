a0846cf2394877c5ad938fcd4f8c157f58e910cd
Larik7 <Larik777taganka@gmail.com>
2023-01-11 01:53:53 +0200

    third commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,70 +1,22 @@
-import player from '@vimeo/player';
-import  throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+import Throttle from 'lodash.throttle';
 
-// first version
+const iframe = document.querySelector('iframe');
 
-// const iframe = document.querySelector('iframe');
-// const CURRENT_TIME = 'videoplayer-current-time';
-// const player = new Vimeo.Player(iframe);
+const iframePlayer = new Player(iframe);
 
-// player.on('timeupdate', throttle(onPlay, 1000));
+const stoppedOn = localStorage.getItem("videoplayer-current-time");
 
-// function onPlay({ seconds }) {
-// localStorage.setItem(CURRENT_TIME, seconds);
-// }
+const rememberStopPoint = Throttle((seconds) => {
+  localStorage.setItem("videoplayer-current-time", seconds);
+}, 1000, { trailing: false });
 
-// setCurrentTime()
-// function setCurrentTime(){
-// const savedTime = localStorage.getItem(CURRENT_TIME);
-// if(!savedTime){
-// return
-// }
-// player.setCurrentTime(savedTime)
-// }
+const onPlay = ({seconds}) => {
+  rememberStopPoint(seconds);
+};
 
+if (stoppedOn) {
+  iframePlayer.setCurrentTime(stoppedOn);
+}
 
-
-
-
-
-// const iframe = document.querySelector('iframe');
-// const CURRENT_TIME = 'videoplayer-current-time';
-// // const iframePlayer = new Vimeo.Player(iframe);
-// const player = new Vimeo.Player(iframe);
-
-// player.on('timeupdate', function (currentTime) {
-//     const seconds = currentTime.seconds;
-//     localStorage.setItem(CURRENT_TIME, JSON.stringify(seconds));
-// });
-
-// player.on('play', onPlay);
-// player.on('eventName', function(data) {
-//     // data is an object containing properties specific to that event
-// });
-
-// setCurrentTime
-
-// player.setCurrentTime(30.456).then(function(seconds) {
-//     // seconds = the actual time that the player seeked to
-// }).catch(function(error) {
-//     switch (error.name) {
-//         case 'RangeError':
-//             // the time was less than 0 or greater than the video’s duration
-//             break;
-
-//         default:
-//             // some other error occurred
-//             break;
-//     }
-// });
-
-// getCurrentTime()
-
-// player.getCurrentTime().then(function(seconds) {
-//     // seconds = the current playback position
-// }).catch(function(error) {
-//     // an error occurred
-// });
-
-
-// console.log(Player);
+iframePlayer.on('timeupdate', onPlay);
