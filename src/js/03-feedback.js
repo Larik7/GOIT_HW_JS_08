1dd3d20de122314b3eafc710394402fd4e6f6ec1
Larik7 <Larik777taganka@gmail.com>
2023-01-15 14:38:09 +0200

    second commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,18 +3,18 @@ import throttle from "lodash.throttle";
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 const form = document.querySelector('form');
 
-const save = (key, value) => {
+const save = (LOCALSTORAGE_KEY, value) => {
   try {
     const serializedState = JSON.stringify(value);
-    localStorage.setItem(key, serializedState);
+    localStorage.setItem(LOCALSTORAGE_KEY, serializedState);
   } catch (error) {
     console.error("Set state error: ", error.message);
   }
 };
 
-const load = key => {
+const load = LOCALSTORAGE_KEY => {
   try {
-    const serializedState = localStorage.getItem(key);
+    const serializedState = localStorage.getItem(LOCALSTORAGE_KEY);
     return serializedState === null ? undefined : JSON.parse(serializedState);
   } catch (error) {
     console.error("Get state error: ", error.message);
